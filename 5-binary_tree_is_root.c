f162c8d32e94c234966dc8a03be27320eb1af614
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-02-02 05:33:00 +0300

    Task 5: Is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - CHeck if node is root
+ * @node: Node to check
+ *
+ * Return: 1 or 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	int i = 0;
+
+	if (node->parent == NULL)
+	{
+		i = 1;
+	}
+	return (i);
+}
